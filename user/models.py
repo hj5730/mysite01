ad6a52bb16dfe58dacc4977de21693004944c791
hj5730 <hj5730@naver.com>
2021-04-08 15:11:13 +0900

    board app 만들기 #3

--- user/models.py
+++ user/models.py
@@ -1,19 +1,33 @@
-# 크게 함수가 2개 있음
-
 from MySQLdb import connect, OperationalError
 from MySQLdb.cursors import DictCursor
 
-def findbyno():
-    sql = 'select no, name, email, gender from user where no = %s'
-    pass
 
-def findby_email_and_password(email, password): # app 에다가 주는 것
+def findbyno(no):
+    try:
+        db = conn()
+        cursor = db.cursor(DictCursor)
+
+        sql = 'select no, name, email, gender from user where no=%s'
+        cursor.execute(sql, (no,))
+        result = cursor.fetchone()
+
+        # 자원 정리
+        cursor.close()
+        db.close()
+
+        return result
+
+    except OperationalError as e:
+        print(f'error: {e}')
+
+
+def findby_email_and_password(email, password):
     try:
         # 연결
         db = conn()
 
         # cursor 생성
-        cursor = db.cursor(DictCursor)  # cursor: 쿼리 날려주는 것
+        cursor = db.cursor(DictCursor)
 
         # SQL 실행
         sql = 'select no, name from user where email=%s and password = %s'
@@ -32,13 +46,14 @@ def findby_email_and_password(email, password): # app 에다가 주는 것
     except OperationalError as e:
         print(f'error: {e}')
 
-def insert(name, email, password, gender): # app 에서 가져오는 것
+
+def insert(name, email, password, gender):
     try:
         # 연결
         db = conn()
 
         # cursor 생성
-        cursor = db.cursor()  # cursor: 쿼리 날려주는 것
+        cursor = db.cursor()
 
         # SQL 실행
         sql = 'insert into user values(null, %s, %s, %s, %s, now())'
@@ -58,8 +73,35 @@ def insert(name, email, password, gender): # app 에서 가져오는 것
         print(f'error: {e}')
 
 
-def update(email):
-    pass
+def update(no, name, password, gender):
+    try:
+        db = conn()
+        cursor = db.cursor()
+
+        if password == '':
+            sql = '''
+                update user
+                   set name=%s, gender=%s
+                 where no=%s 
+            '''
+            t = (name, gender, no)
+        else:
+            sql = '''
+                update user
+                   set name=%s, password=%s, gender=%s
+                 where no=%s 
+            '''
+            t = (name, password, gender, no)
+
+        cursor.execute(sql, t)
+        db.commit()
+
+        # 자원 정리
+        cursor.close()
+        db.close()
+
+    except OperationalError as e:
+        print(f'error: {e}')
 
 
 def conn():
